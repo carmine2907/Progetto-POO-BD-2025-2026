e76a5353d362b4e279b468655157c47ae5ff1e5c
carmine2907 <carmine.romano18@studenti.unina.it>
2026-05-02 12:38:16 +0200

    prova utente

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -4,7 +4,7 @@ package model;
 public class Utente {
     private  String login;
     private String password;
-
+    private String nome;
     public Utente(String login, String password) {
         this.login = login;
         this.password = password;
