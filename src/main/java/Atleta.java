b2ad82310b2ec8965f7847cbb8bd30349a879acf
Mario-sdino0408 <mar.sdino@studenti.unina.it>
2026-05-06 12:50:53 +0200

    paginreg

--- src/main/java/Atleta.java
+++ src/main/java/Atleta.java
@@ -4,6 +4,7 @@ public class Atleta extends Utente
 {
     private Date dataNascita;
     private String ruolo;
+    private boolean pagamentoInReg = false;
 
     public Atleta (String login, String password, String nome, String cognome)
     {
@@ -17,6 +18,15 @@ public class Atleta extends Utente
 
     public String getRuolo() { return ruolo; }
     public void setRuolo(String ruolo) { this.ruolo = ruolo; }
+
+    public boolean pagamentoInRegola(boolean pagamentoInReg)
+    {
+        if(pagamentoInReg == true)
+        {
+            System.out.println("Il pagamento è in regola");
+        }
+        return false;
+    }
 
     public String toString() { return super.toString() +dataNascita; }
 }
